c1b6a3c9e6133c262c0dc03a5eaa753c009dec87
Petr Bouchal <pbouchal@gmail.com>
2014-09-09 22:32:45 +0100

    update style of jobs shiny app

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 ## Date query: select max(datetime) as date from data
 
 library(shiny)
+library(RCurl)
 
 url_data <- 'https://api.morph.io/petrbouchal/GovJobsCZ/data.csv?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20%22%3Ca%20href%3D%22%22%22%20%7C%7C%20joburl%20%7C%7C%20%22%22%22%3E%22%20%7C%7C%20jobtitle%20%7C%7C%20%22%3C%2Fa%3E%22%20as%20Pozice%2C%20dept%20as%20Ministerstvo%20from%20%27data%27%20where%20datetime%20%3D%20(select%20max(datetime)%20from%20data)%20order%20by%20Ministerstvo'
 url_date <- 'https://api.morph.io/petrbouchal/GovJobsCZ/data.csv?key=N4S7F3oGM4jPyicp%2B2mx&query=select%20max(datetime)%20as%20date%20from%20data'
@@ -15,18 +16,18 @@ shinyApp(
   ),
   server=function(input, output) {
     tmpFile <- tempfile()
-#     download.file(url_data, destfile = tmpFile, method = "curl")    
-    download.file(url_data, destfile = tmpFile)
-    data <- read.csv(tmpFile)
+    tmpFile <- getURL(url_data)
+#     download.file(url_data, destfile = tmpFile)
+    data <- read.csv(text=tmpFile, fileEncoding = "UTF-8")
     tmpFile2 <- tempfile()
-    download.file(url_date, destfile = tmpFile2)
-    date <- read.csv(tmpFile2)
+    tmpFile2 <- getURL(url_date)
+    date <- read.csv(text=tmpFile2)
     datum <- strptime(date$date, '%Y-%m-%d')
     datum <- strftime(date$date, '%d. %m. %Y')
     deptcount <- length(unique(data$Ministerstvo))
     jobcount <- length(unique(data$Pozice))
     output$counttext <- renderText(paste0(' Nalezeno ',jobcount,' nabidek od ',
                                    deptcount,' uradu. Naposledy zkontrolovano ', datum,'.'))
-    output$data <- renderDataTable(data,options = list(bLengthChange=F))
+    output$data <- renderDataTable(data,options = list(lengthChange=F))
   }
 )
